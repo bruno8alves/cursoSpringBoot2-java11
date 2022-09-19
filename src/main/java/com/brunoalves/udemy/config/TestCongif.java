68353840f434398a833c9a9e889b9d7ddf43b38d
Bruno Alves <bruno.8.alves@gmail.com>
2022-09-19 15:37:26 -0700

    Product

--- src/main/java/com/brunoalves/udemy/config/TestCongif.java
+++ src/main/java/com/brunoalves/udemy/config/TestCongif.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.brunoalves.udemy.entities.Category;
 import com.brunoalves.udemy.entities.Order;
+import com.brunoalves.udemy.entities.Product;
 import com.brunoalves.udemy.entities.User;
 import com.brunoalves.udemy.enums.OrderStatus;
 import com.brunoalves.udemy.repositories.CategoryRepository;
 import com.brunoalves.udemy.repositories.OrderRepository;
+import com.brunoalves.udemy.repositories.ProductRepository;
 import com.brunoalves.udemy.repositories.UserRepository;
 
 @Configuration
@@ -31,6 +33,10 @@ public class TestCongif implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -52,6 +58,12 @@ public class TestCongif implements CommandLineRunner{
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
 		
+		Product p1 = new Product(null,"The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null,"Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null,"Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null,"PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null,"Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		
 		
